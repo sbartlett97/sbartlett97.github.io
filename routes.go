649929934ec07bde7e68dacec21788c624a187da
Samuel Bartlett <sam.bartlett858@gmail.com>
2023-03-13 22:03:04 +0000

    add some basic comments

--- routes.go
+++ routes.go
@@ -13,10 +13,14 @@ import (
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
+	// All go htt.Request objects have a built in context handler
 	ctx := req.Context()
 	fmt.Println("[server]: Hello handler started")
 	
+	// defer calls allow us to schedule a function to run when the 
+	// function exits
 	defer fmt.Println("[server]: Hello handler ended")
+
 
 	select {
 	case <- time.After(10 * time.Second):
@@ -32,7 +36,13 @@ func hello(w http.ResponseWriter, req *http.Request) {
 
 func headers(w http.ResponseWriter, req *http.Request) {
 
+	// simple loop to iterate over a both 
+	// keys and values of dictionary like object
     for name, headers := range req.Header {
+
+		// for loop runs over the header content
+		// lopos over a range in go return both the item and
+		// the iteration count of the loop unlike python
         for _, h := range headers {
             fmt.Fprintf(w, "%v: %v\n", name, h)
         }
