cbe57e7b7fd6af543c6870ca3fdaff2991787028
Samuel Bartlett <sam.bartlett858@gmail.com>
2023-03-14 07:38:21 +0000

    small refactoring

--- server.go
+++ server.go
@@ -9,15 +9,15 @@ func main() {
 
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./static"))
+
     mux.HandleFunc("/hello", hello)
     mux.HandleFunc("/headers", headers)
-
+	mux.HandleFunc("/", serveTemplate)
 
 	// without striping the /static/ preix for static urls, the server will treat
 	// /static as a folder in the route of the filesystem
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	mux.HandleFunc("/", serveTemplate)
 	srv := &http.Server{
 		Addr: 	":8090",
 		Handler: 	mux,
