06c806d0e3be731104ce04a9bfe1270bd29287ff
Samuel Bartlett <sam.bartlett858@gmail.com>
2023-03-15 17:39:17 +0000

    add new file handlers for html template

--- server.go
+++ server.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"path/filepath"	
-    "net/http"
 	"log"
+	"net/http"
 	"os"
+	"path/filepath"
 )
 
 var (
-	StaticDir string
-	LayoutPath string 
+	StaticDir   string
+	LayoutPath  string
 	TemplateDir string
 )
-
-
 
 func main() {
 
@@ -22,21 +20,28 @@ func main() {
 
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./" + StaticDir))
-
-    mux.HandleFunc("/hello", hello)
-    mux.HandleFunc("/headers", headers)
+	CssFs := http.FileServer(http.Dir("./css"))
+	ImgFs := http.FileServer(http.Dir("./images"))
+	FontsFs := http.FileServer(http.Dir("./fonts"))
+	ScriptsFs := http.FileServer(http.Dir("./scripts"))
+	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/headers", headers)
 	mux.HandleFunc("/", serveTemplate)
 
 	// without striping the /static/ preix for static urls, the server will treat
 	// /static as a folder in the route of the filesystem
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.Handle("/css/", http.StripPrefix("/css/", CssFs))
+	mux.Handle("/images/", http.StripPrefix("/images/", ImgFs))
+	mux.Handle("/fonts/", http.StripPrefix("/fonts/", FontsFs))
+	mux.Handle("/js/", http.StripPrefix("/js/", ScriptsFs))
 
 	srv := &http.Server{
-		Addr: 	":8090",
-		Handler: 	mux,
+		Addr:    ":8090",
+		Handler: mux,
 	}
 
-    err := srv.ListenAndServe()
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
